b431754dffea55ab67f8011690b5bbd6f5e59dba
dantedif <manuel_agh@hotmail.com>
2018-04-02 21:57:31 -0600

    people skills

--- src/actions/people/people.js
+++ src/actions/people/people.js
@@ -1,18 +1,16 @@
 import ActionTypes from './action_types';
 import axios from 'axios';
-import API from '../../constants/gfAPI';
+import {API} from "../../constants/gfAPI";
+
 
 export const GetAllPeople = ()=>{
     return dispatch =>{
         dispatch(request_all_people());
-
-        axios.get(API.GetAllPeopleIDFullName)
+        axios.get(API.GetAllPeopleInformation)
             .then((response)=>{
-                console.log("ACCION AL RETONRAR RESULTADO");
                 dispatch(request_all_people_Success(response));
             })
             .catch((error)=>{
-                console.log("ACCION AL RETONRAR ERROR");
                 dispatch(request_all_people_Error(error));
             });
     }
--- src/components/people/people_skills.js
+++ src/components/people/people_skills.js
@@ -1,47 +1,134 @@
 import React from 'react';
 import {
     Card,  CardBody,
-    CardTitle,   Row, Col
+    CardTitle,   Row, Col,Badge,Table
 } from 'reactstrap';
 import {Typeahead} from "react-bootstrap-typeahead";
 import {Field, reduxForm} from "redux-form";
+import _ from 'lodash';
+import Editable from 'react-x-editable';
 
-let renderFieldTypeAhead = ({input, placeholder, options,  meta :{ touched, error }}) => {
-    return(
-        <div>
-            <Typeahead
-                {...input}
-                labelKey="billing_state"
-                options={options}
-                placeholder={placeholder}
-            />
-        </div>
-    );
-};
 
-let PeopleSkills =props=> {
-    let people_source = ["a","b"];
-        return (
-            <div className="animated fadeIn">
-             <Card>
-                 <CardBody>
-                     <CardTitle>
-                         <p className="text-gray-500">Welcome to the skill assignment page <small>You can search for a person and view, edit or delete the assigned skills.</small></p>
-                     </CardTitle>
 
-                     <Row className="mb-md-3">
-                         <Col>
-                             <Field type="text" name="searchBox"
-                                    placeholder="Search people by name"
-                                    options={people_source}
-                                    component={renderFieldTypeAhead}
-                             />
-                         </Col>
-                     </Row>
-                 </CardBody>
-             </Card>
+class PeopleSkills extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            selected:null
+        }
+        this.props.GetAllPeople_c();
+        this.renderCardPeopleSelected = this.renderCardPeopleSelected.bind(this);
+        //   this.renderFieldTypeAhead = this.renderFieldTypeAhead(this);
+    }
+    renderFieldTypeAhead = ({input, placeholder, options}) => {
+        if(options !== undefined){
+            return(
+                <div>
+                    <Typeahead
+                        {...input}
+                        options={options}
+                        labelKey="FullName"
+                        placeholder={placeholder}
+                        onChange={(selected)=> this.setPeopleSelected(selected)}
+                    />
+                </div>
+            );
+        }
+        else
+            return(<div></div>);
+    };
+    setPeopleSelected(selected){
+        this.setState({selected:selected});
+    }
+    renderCardPeopleSelected(selected){
+        if(!_.isNil(selected)){
+            console.log("Selected ", selected);
+            return(
+                <Row>
+                    <Col xs="6" md="3">
+                        <img src={selected.UrlPhoto} alt={selected.FullName}/>
+                    </Col>
+                    <Col xs="6" md="9">
+                        {this.renderSkillsTable(selected)}
+                    </Col>
+                </Row>
+            );
+        }
+    }
+    renderSkillsTable(selected){
+        return(
+            <div>
+                <Table responsive>
+                    <thead>
+                    <tr>
+                        <th>Skill</th>
+                        <th>Level</th>
+
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {this.renderSkillsTableRows(selected.Skills)}
+                    </tbody>
+                </Table>
             </div>
         );
+    }
+    renderSkillsTableRows(skills){
+        return(
+            _.map(skills, skill =>{
+                return(
+                        <tr key={skill.SkillID}>
+                            <td>{skill.SkillName}</td>
+                            <td>{skill.Level}</td>
+                            <td><Editable
+                                name="username"
+                                dataType="text"
+                                mode="popup"
+                                title="Please enter username"
+                            /></td>
+                        </tr>
+                );
+            })
+        );
+    }
+
+    render(){
+        const {AllPeople} = this.props;
+        const selectedValue = this.state.selected != undefined ? this.state.selected[0] : undefined;
+        return (
+            <div className="animated fadeIn">
+                <Row>
+                    <Col>
+                        <Card>
+                            <CardBody>
+                                <Row>
+                                    <Col xs="12">
+                                        <CardTitle>
+                                            <p className="text-gray-500">Welcome to the skill assignment page <small>You can search for a person and view, edit or delete the assigned skills.</small></p>
+                                        </CardTitle>
+                                    </Col>
+                                    <Col xs="12">
+                                        <Field type="text" name="searchBox"
+                                               placeholder="Search people by name"
+                                               options={AllPeople}
+                                               component={this.renderFieldTypeAhead}
+                                        />
+                                    </Col>
+                                </Row>
+                                <div className="container-fluid">
+                                    <Row>
+                                        <Col xs="12">
+                                            {this.renderCardPeopleSelected(selectedValue)}
+                                        </Col>
+                                    </Row>
+                                </div>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+            </div>
+        );
+    }
 }
 
 export default reduxForm({form:"PeopleSkills"})(PeopleSkills);
